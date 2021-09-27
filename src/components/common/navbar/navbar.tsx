28f7c5724ece9b87bee91a136bfad62b38fb3626
book.siras <book.siras@bitkub.com>
2021-09-27 16:28:55 +0700

    add Navbar

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -1,9 +1,32 @@
+import React from "react";
+import { Menu } from "antd";
+import { Link, useHistory } from "react-router-dom";
+import "./navbar.scss";
+
 const Navbar: React.FC = () => {
+
+    const history = useHistory();
+
     return (
-        <>
-        
-        </>
-    )
+      <div className="navbar-container">
+        <div className="desktop height">
+          <Menu mode="horizontal">
+            <Menu.Item key="1" className="desktop-logo">
+              <Link to="/home">TRAVELLESTGO</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/transport">Transport</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link to="/profile">Profile</Link>
+            </Menu.Item>
+            {/* <Menu.Item key="6" className="logout-tab">
+              <div onClick={showLogoutModal}>Logout</div>
+            </Menu.Item> */}
+          </Menu>
+        </div>
+      </div>
+    );
 }
 
 export default Navbar
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,20 @@
 import { CardBox } from "../../components"
+import Navbar from "../../components/common/navbar/navbar"
 import './home.scss'
+import React from "react";
 
 const Home: React.FC = () => {
     return (
-        <div className="home-container">
-        home
-        <h1>Recommended Routes</h1>
-        <CardBox />
-        <CardBox />
-        <CardBox />
-        <CardBox />
+        <div>
+            <Navbar/>
+            <div className="home-container">
+            home
+                <h1>Recommended Routes</h1>
+                <CardBox />
+                <CardBox />
+                <CardBox />
+                <CardBox />
+            </div>
         </div>
     )
 }
